6eda0b7fcc4ba607e602a718bb425eaf7ebd1174
Perma_4thgenna <glendon4b1@gmail.com>
2018-10-21 13:18:47 -0500

    Exercise 3

--- maze.js
+++ maze.js
@@ -1,4 +1,4 @@
-
+var result = false;
 
 function setUpEvents(){
 	let allBoundaries = document.getElementsByClassName("boundary");
@@ -7,13 +7,28 @@ function setUpEvents(){
 			allred();
 		});
 	}
+	
+	let end = document.getElementById("end");
+	end.addEventListener("mouseover", function(){
+		finish();
+	})
 }
 
 function allred(){
+	result = true;
 	let allBoundaries = document.getElementsByClassName("boundary");
 	for (let i = 0; i < allBoundaries.length; i++) {		
 		allBoundaries[i].classList.add("youlose");
 	}
+}
+
+function finish(){
+	if (result){
+		alert("You lose");
+	} else {
+		alert("You win");
+	}
+	
 }
 
 
